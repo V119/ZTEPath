6e28a5fd1669f6630bb277748e53c56762438e76
yanghao <928131755@qq.com>
2017-05-07 22:53:33 +0800

    邻接矩阵完成

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
+import xjtu.se.hmbb.dataStruct.AMGraph;
+
+import java.io.File;
+
 /**
  * Created by haoyang on 2017/5/6.
  */
 public class Test {
+    public static void main(String[] args) {
+        File file = new File("/Users/haoyang/IdeaProjects/ZTEPath/src/graph.txt");
+        AMGraph am = new AMGraph(file);
+    }
 }
--- src/xjtu/se/hmbb/dataStruct/AMGraph.java
+++ src/xjtu/se/hmbb/dataStruct/AMGraph.java
@@ -13,59 +13,107 @@ public class AMGraph {
     private int start;                              //开始节点的下标
     private int end;                                //结束节点的下标
     private List<Integer> fruitRoomList;            //水果间，必经过的节点下标
-    private List<Integer[]> anteaterPath;           //食蚁兽所在的边
-    private List<Integer[]> rewardPath;              //带有奖励的边
+    private List<int[]> anteaterPath;           //食蚁兽所在的边
+    private List<int[]> rewardPath;              //带有奖励的边
 
-    public AMGraph(File file) throws IOException {
+    public AMGraph(File file) {
         this.numOfEdges = 0;
         this.vertexList = new ArrayList<>();
+        this.fruitRoomList = new ArrayList<>();
+        this.anteaterPath = new ArrayList<>();
+        this.rewardPath = new ArrayList<>();
 
         int sentry = 0;
 
         Map<String, Integer> nodeToIndex = new HashMap<>();
 
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String line = "";
-        while((line = reader.readLine()) != null) {
-            if(line.trim().equals("")) {
-                sentry++;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                if(line.trim().equals("")) {
+                    sentry++;
+                    continue;
+                }
+
+                String[] charList = line.trim().split(" ");
+                // 如果哨兵值为0，获取节点个数,并初始化权重的二维数组
+                if(sentry == 0) {
+                    int numOfIndex = Integer.parseInt(charList[0]);
+                    this.edges = new int[numOfIndex][numOfIndex];
+                } else if(sentry == 1) {  // 如果哨兵值为0，读取边和节点
+                    int startIndex = 0;
+                    int endIndex = 0;
+
+                    if(nodeToIndex.containsKey(charList[0])) {
+                        startIndex = nodeToIndex.get(charList[0]);
+                    } else {
+                        startIndex = this.vertexList.size();
+
+                        //获取初始节点和结束节点的下标
+                        if (charList[0].equals("S")) {
+                            this.start = startIndex;
+                        } else if (charList[0].equals("E")) {
+                            this.end = startIndex;
+                        }
+
+                        nodeToIndex.put(charList[0], startIndex);
+                        this.vertexList.add(charList[0]);
+                    }
+
+                    if(nodeToIndex.containsKey(charList[1])) {
+                        endIndex = nodeToIndex.get(charList[1]);
+                    } else {
+                        endIndex = this.vertexList.size();
+
+                        if (charList[1].equals("S")) {
+                            this.start = endIndex;
+                        } else if (charList[1].equals("E")) {
+                            this.end = endIndex;
+                        }
+
+                        nodeToIndex.put(charList[1], endIndex);
+                        this.vertexList.add(charList[1]);
+                    }
+
+                    this.numOfEdges++;
+
+                    //将边的权重加入edge中
+                    int weight = Integer.parseInt(charList[2]);
+                    this.edges[startIndex][endIndex] = weight;
+                    this.edges[endIndex][startIndex] = weight;
+                } else if(sentry == 2) { // 如果哨兵值为1，读取食蚁兽路径
+                    int[] anteaterPathList = new int[2];
+                    anteaterPathList[0] = nodeToIndex.get(charList[0]);
+                    anteaterPathList[1] = nodeToIndex.get(charList[1]);
+                    this.anteaterPath.add(anteaterPathList);
+                } else if(sentry == 3) { //如果哨兵值为2，读取奖励的必过路径
+                    int[] rewardPathList = new int[2];
+                    rewardPathList[0] = nodeToIndex.get(charList[0]);
+                    rewardPathList[1] = nodeToIndex.get(charList[1]);
+                    this.rewardPath.add(rewardPathList);
+                } else if(sentry == 4) { //如果哨兵值为3， 读取水果间
+                    for(String node: charList) {
+                        this.fruitRoomList.add(nodeToIndex.get(node));
+                    }
+                }
             }
-
-            String[] charList = line.trim().split(" ");
-            // 如果哨兵值为0，读取边和节点
-            if(sentry == 0) {
-                int startIndex = 0;
-                int endIndex = 0;
-
-                if(nodeToIndex.containsKey(charList[0])) {
-                    startIndex = nodeToIndex.get(charList[0]);
-                } else {
-                    startIndex = vertexList.size();
-                    nodeToIndex.put(charList[0], startIndex);
-                    vertexList.add(charList[0]);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
                 }
-
-                if(nodeToIndex.containsKey(charList[1])) {
-                    endIndex = nodeToIndex.get(charList[1]);
-                } else {
-                    endIndex = vertexList.size();
-                    nodeToIndex.put(charList[1], endIndex);
-                    vertexList.add(charList[1]);
-                }
-
-                //将边的权重加入edge中
-                int weight = Integer.getInteger(charList[2]);
-                edges[startIndex][endIndex] = weight;
-                edges[endIndex][startIndex] = weight;
-                numOfEdges++;
-            } else if(sentry == 1) { // 如果哨兵值为1，读取食蚁兽路径
-
-            } else if(sentry == 2) { //如果哨兵值为2，读取奖励的必过路径
-
-            } else if(sentry == 3) { //如果哨兵值为3， 读取水果间
-
             }
         }
+
 
     }
 
@@ -117,19 +165,19 @@ public class AMGraph {
         this.fruitRoomList = fruitRoomList;
     }
 
-    public List<Integer[]> getAnteaterPath() {
+    public List<int[]> getAnteaterPath() {
         return anteaterPath;
     }
 
-    public void setAnteaterPath(List<Integer[]> anteaterPath) {
+    public void setAnteaterPath(List<int[]> anteaterPath) {
         this.anteaterPath = anteaterPath;
     }
 
-    public List<Integer[]> getRewardPath() {
+    public List<int[]> getRewardPath() {
         return rewardPath;
     }
 
-    public void setRewardPath(List<Integer[]> rewardPath) {
+    public void setRewardPath(List<int[]> rewardPath) {
         this.rewardPath = rewardPath;
     }
 }
